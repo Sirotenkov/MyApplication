990eb3b06848a091e4d5025e296e65d6cf3c27a0
Alex <aleksej.sirotenkov@simaster.ru>
2024-09-30 10:23:36 +0300

    Добавлены переводы текстовых строк. Доступные языки - русский, английский

--- translations/ru.template.MyApplication-ru.ts
+++ translations/ru.template.MyApplication-ru.ts
@@ -2,36 +2,6 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="ru">
 <context>
-    <name>AboutPage</name>
-    <message>
-        <location filename="../qml/pages/AboutPage.qml" line="20"/>
-        <source>About Application</source>
-        <translation>О приложении</translation>
-    </message>
-    <message>
-        <location filename="../qml/pages/AboutPage.qml" line="30"/>
-        <source>#descriptionText</source>
-        <translation>&lt;p&gt;Короткое описание моего приложения для ОС Аврора&lt;/p&gt;</translation>
-    </message>
-    <message>
-        <location filename="../qml/pages/AboutPage.qml" line="35"/>
-        <source>3-Clause BSD License</source>
-        <translation>Лицензия 3-Clause BSD</translation>
-    </message>
-    <message>
-        <location filename="../qml/pages/AboutPage.qml" line="45"/>
-        <source>#licenseText</source>
-        <translation>&lt;p&gt;&lt;i&gt;Copyright (C) 2022 ru.template&lt;/i&gt;&lt;/p&gt;
-&lt;p&gt;Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:&lt;/p&gt;
-&lt;ol&gt;
-	&lt;li&gt;Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.&lt;/li&gt;
-	&lt;li&gt;Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.&lt;/li&gt;
-	&lt;li&gt;Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.&lt;/li&gt;
-&lt;/ol&gt;
-&lt;p&gt;THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS &quot;AS IS&quot; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.&lt;/p&gt;</translation>
-    </message>
-</context>
-<context>
     <name>DefaultCoverPage</name>
     <message>
         <location filename="../qml/cover/DefaultCoverPage.qml" line="10"/>
@@ -44,139 +14,107 @@
     <message>
         <location filename="../qml/pages/InformationPage.qml" line="11"/>
         <source>Информация</source>
-        <translation type="unfinished"></translation>
+        <translation>Информация</translation>
     </message>
     <message>
         <location filename="../qml/pages/InformationPage.qml" line="12"/>
-        <source>Плодробная информация о пользователе</source>
-        <translation type="unfinished"></translation>
+        <source>Подробная информация о пользователе</source>
+        <translation>Подробная информация о пользователе</translation>
     </message>
     <message>
-        <location filename="../qml/pages/InformationPage.qml" line="15"/>
-        <source>Information</source>
-        <translation type="unfinished"></translation>
+        <location filename="../qml/pages/InformationPage.qml" line="44"/>
+        <source>Заполните, пожалуйста, более подробные сведения о себе</source>
+        <translation>Заполните, пожалуйста, более подробные сведения о себе</translation>
     </message>
     <message>
-        <location filename="../qml/pages/InformationPage.qml" line="46"/>
-        <source>Заполните, пожалуйста, более подробные сведения о себе на данной странице</source>
-        <translation type="unfinished"></translation>
+        <location filename="../qml/pages/InformationPage.qml" line="57"/>
+        <source>Ваше имя</source>
+        <translation>Ваше имя</translation>
     </message>
     <message>
         <location filename="../qml/pages/InformationPage.qml" line="59"/>
-        <source>Ваше имя</source>
-        <translation type="unfinished"></translation>
+        <source>Имя пользователя</source>
+        <translation>Имя пользователя</translation>
     </message>
     <message>
-        <location filename="../qml/pages/InformationPage.qml" line="61"/>
-        <source>Имя пользователя</source>
-        <translation type="unfinished"></translation>
+        <location filename="../qml/pages/InformationPage.qml" line="72"/>
+        <source>Ваша фамилия</source>
+        <translation>Ваша фамилия</translation>
     </message>
     <message>
         <location filename="../qml/pages/InformationPage.qml" line="74"/>
-        <source>Ваша фамилия</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/pages/InformationPage.qml" line="76"/>
         <source>Фамилия пользователя</source>
-        <translation type="unfinished"></translation>
+        <translation>Фамилия пользователя</translation>
     </message>
     <message>
-        <location filename="../qml/pages/InformationPage.qml" line="90"/>
-        <source>Вы являетесь гражданином РФ?</source>
-        <translation type="unfinished"></translation>
+        <location filename="../qml/pages/InformationPage.qml" line="88"/>
+        <source>Вы являетесь гражданином РФ</source>
+        <translation>Вы являетесь гражданином РФ</translation>
     </message>
     <message>
-        <location filename="../qml/pages/InformationPage.qml" line="100"/>
+        <location filename="../qml/pages/InformationPage.qml" line="98"/>
         <source>Да</source>
-        <translation type="unfinished"></translation>
+        <translation>Да</translation>
     </message>
     <message>
-        <location filename="../qml/pages/InformationPage.qml" line="100"/>
+        <location filename="../qml/pages/InformationPage.qml" line="98"/>
         <source>Нет</source>
-        <translation type="unfinished"></translation>
+        <translation>Нет</translation>
     </message>
     <message>
-        <location filename="../qml/pages/InformationPage.qml" line="101"/>
+        <location filename="../qml/pages/InformationPage.qml" line="99"/>
         <source>Наличие гражданства РФ</source>
-        <translation type="unfinished"></translation>
+        <translation>Наличие гражданства РФ</translation>
     </message>
     <message>
-        <location filename="../qml/pages/InformationPage.qml" line="108"/>
+        <location filename="../qml/pages/InformationPage.qml" line="106"/>
         <source>Введите адрес электронной почты</source>
-        <translation type="unfinished"></translation>
+        <translation>Введите адрес электронной почты</translation>
     </message>
     <message>
-        <location filename="../qml/pages/InformationPage.qml" line="119"/>
-        <source>username@yandex.ru</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/pages/InformationPage.qml" line="120"/>
-        <source>E-mail</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/pages/InformationPage.qml" line="133"/>
+        <location filename="../qml/pages/InformationPage.qml" line="131"/>
         <source>Введите номер мобильного телефона</source>
-        <translation type="unfinished"></translation>
+        <translation>Введите номер мобильного телефона</translation>
     </message>
     <message>
-        <location filename="../qml/pages/InformationPage.qml" line="143"/>
+        <location filename="../qml/pages/InformationPage.qml" line="141"/>
         <source>Номер телефона</source>
-        <translation type="unfinished"></translation>
+        <translation>Номер телефона</translation>
     </message>
     <message>
-        <location filename="../qml/pages/InformationPage.qml" line="146"/>
-        <source>Mobile phone number</source>
-        <translation type="unfinished"></translation>
+        <location filename="../qml/pages/InformationPage.qml" line="144"/>
+        <source>Номер мобильного телефона</source>
+        <translation>Номер мобильного телефона</translation>
     </message>
     <message>
-        <location filename="../qml/pages/InformationPage.qml" line="158"/>
+        <location filename="../qml/pages/InformationPage.qml" line="156"/>
         <source>Введите что-нибудь ещё</source>
-        <translation type="unfinished"></translation>
+        <translation>Введите что-нибудь ещё</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/InformationPage.qml" line="167"/>
+        <source>Информация в произвольном виде</source>
+        <translation>Информация в произвольном виде</translation>
     </message>
     <message>
         <location filename="../qml/pages/InformationPage.qml" line="169"/>
-        <source>Информация в произвольном виде</source>
-        <translation type="unfinished"></translation>
+        <source>Любая информация</source>
+        <translation>Любая информация</translation>
     </message>
     <message>
-        <location filename="../qml/pages/InformationPage.qml" line="171"/>
-        <source>Любая информация...</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/pages/InformationPage.qml" line="172"/>
+        <location filename="../qml/pages/InformationPage.qml" line="170"/>
         <source>Дополнительная информация</source>
-        <translation type="unfinished"></translation>
+        <translation>Дополнительная информация</translation>
     </message>
     <message>
-        <location filename="../qml/pages/InformationPage.qml" line="192"/>
+        <location filename="../qml/pages/InformationPage.qml" line="190"/>
         <source>Отправить сведения</source>
-        <translation type="unfinished"></translation>
+        <translation>Отправить сведения</translation>
     </message>
     <message>
-        <location filename="../qml/pages/InformationPage.qml" line="205"/>
+        <location filename="../qml/pages/InformationPage.qml" line="203"/>
         <source>Предыдущая страница</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
-<context>
-    <name>MainPage</name>
-    <message>
-        <location filename="../qml/pages/MainPage.qml" line="10"/>
-        <source>Template</source>
-        <translation>Шаблон</translation>
-    </message>
-</context>
-<context>
-    <name>MyApplication</name>
-    <message>
-        <location filename="../qml/MyApplication.qml" line="8"/>
-        <location filename="../qml/MyApplication.qml" line="17"/>
-        <source>Main</source>
-        <translation type="unfinished"></translation>
+        <translation>Предыдущая страница</translation>
     </message>
 </context>
 <context>
@@ -184,48 +122,39 @@
     <message>
         <location filename="../qml/pages/RegisterPage.qml" line="11"/>
         <source>Регистрация</source>
-        <translation type="unfinished"></translation>
+        <translation>Регистрация</translation>
     </message>
     <message>
         <location filename="../qml/pages/RegisterPage.qml" line="12"/>
         <source>Страница регистрации пользователя</source>
-        <translation type="unfinished"></translation>
+        <translation>Страница регистрации пользователя</translation>
     </message>
     <message>
-        <location filename="../qml/pages/RegisterPage.qml" line="16"/>
-        <location filename="../qml/pages/RegisterPage.qml" line="68"/>
-        <source>Login</source>
-        <translation type="unfinished">Логин</translation>
+        <location filename="../qml/pages/RegisterPage.qml" line="40"/>
+        <source>Здравствуйте</source>
+        <translation>Здравствуйте</translation>
     </message>
     <message>
-        <location filename="../qml/pages/RegisterPage.qml" line="41"/>
-        <source>Здравствуйте!</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/pages/RegisterPage.qml" line="52"/>
+        <location filename="../qml/pages/RegisterPage.qml" line="51"/>
         <source>Введите свои регистрационные данные</source>
-        <translation type="unfinished"></translation>
+        <translation>Введите свои регистрационные данные</translation>
     </message>
     <message>
-        <location filename="../qml/pages/RegisterPage.qml" line="66"/>
+        <location filename="../qml/pages/RegisterPage.qml" line="65"/>
+        <location filename="../qml/pages/RegisterPage.qml" line="67"/>
         <source>Логин</source>
-        <translation type="unfinished"></translation>
+        <translation>Логин</translation>
     </message>
     <message>
-        <location filename="../qml/pages/RegisterPage.qml" line="81"/>
+        <location filename="../qml/pages/RegisterPage.qml" line="80"/>
+        <location filename="../qml/pages/RegisterPage.qml" line="82"/>
         <source>Пароль</source>
-        <translation type="unfinished"></translation>
+        <translation>Пароль</translation>
     </message>
     <message>
-        <location filename="../qml/pages/RegisterPage.qml" line="83"/>
-        <source>Password</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../qml/pages/RegisterPage.qml" line="106"/>
+        <location filename="../qml/pages/RegisterPage.qml" line="105"/>
         <source>Зарегистрироваться</source>
-        <translation type="unfinished"></translation>
+        <translation>Зарегистрироваться</translation>
     </message>
 </context>
 </TS>
